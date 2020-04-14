76441540e45b6f6f639c59d9f715d2da515fe613
Franc_zi <31786813+FrancsXiang@users.noreply.github.com>
2020-04-14 10:09:52 +0800

    Create floyd.cpp

---
+++ min_dist/floyd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#define MAXN 502
+using namespace std;
+
+int map[MAXN][MAXN];
+
+int main()
+{
+	int n;
+	cin >> n;
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<n;j++) {
+			cin >> map[i][j];
+		}
+	}
+	for(int k=0;k<n;k++) {
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				map[i][j] = min(map[i][j],map[i][k]+map[k][j]);
+			}
+		}
+	}
+	return 0;
+}

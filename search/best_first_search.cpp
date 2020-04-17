12cc99fbe300e724c155a4a8216f1a7546d5863e
Franc_zi <31786813+FrancsXiang@users.noreply.github.com>
2020-04-17 10:37:46 +0800

    Update best_first_search.cpp

--- search/best_first_search.cpp
+++ search/best_first_search.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <queue>
 #define N 10000
 #define INF 0xffffff
@@ -6,7 +7,7 @@ using namespace std;
 typedef pair<int,int> PII;
 
 struct cmp {
-	bool operator() (const PII &a,const PII &b) { //first为顶点号,second为到此点最短距离
+	bool operator() (const PII &a,const PII &b) { 
 		return a.second > b.second;
 	}
 };
@@ -16,6 +17,7 @@ priority_queue<PII,vector<PII>,cmp> que;
 void bestFirstSearch(int cur,int n,vector<vector<PII>>& edge, int* dist) {
 	for(int i=0;i<n;i++) dist[i] = INF;
 	dist[cur] = 0;
+	vis[cur] = true;
 	que.push({cur,0});
 	while(!que.empty()) {
 		auto item = que.top();
@@ -24,6 +26,7 @@ void bestFirstSearch(int cur,int n,vector<vector<PII>>& edge, int* dist) {
 		int end,val; 
 		for(int i=0;i<edge[start].size();i++) {
 			end = edge[start][i].first;
+			if(vis[end]) continue;
 			val = edge[start][i].second;
 			if(val + dist[start] < dist[end]) que.push({end,dist[end] = val + dist[start]});
 		}
@@ -34,17 +37,16 @@ void bestFirstSearch(int cur,int n,vector<vector<PII>>& edge, int* dist) {
 int main()
 {
 	int start,end,val,cur,n,m;
-	cout << "input the number of vertexes and edges respectively:" << endl;
 	cin >> n >> m;
 	int dist[n];
-	vector<vector<PII>> edge(n);//first为顶点号,second为边权
-	cout << "input the edge(start,end,val):" << endl;
+	bool vis[n];
+	memset(vis,false,sizeof(vis));
+	vector<vector<PII>> edge(n);
 	for(int i=0;i<m;i++) {
 		cin >> start >> end >> val;
 		edge[start].push_back({end,val});
 		edge[end].push_back({start,val});
 	} 
-	cout << "input the start vertex:" << endl;
 	cin >> cur;
 	bestFirstSearch(cur,n,edge,dist);
 	cout << "Start from vertex: " << cur << endl;

a88ec5f4007ab2ec275d7763b4b3114a91b18bfd
Franc_zi <31786813+FrancsXiang@users.noreply.github.com>
2020-04-22 11:13:34 +0800

    Create integer_factorization.cpp

---
+++ math/integer_factorization.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+typedef long long LL;
+
+int main()
+{
+	LL n,cnt;
+	cin >> n;
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) {
+			for (cnt = 0; !(n % i) ; cnt++) n /= i;
+			cout << i << " ";
+		}
+	}
+	if (n != 1) cout << n << endl;
+	return 0;
+}

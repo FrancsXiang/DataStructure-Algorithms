683bf61062b80da75cdad6bbc16cf0fe46b16f40
Franc_zi <31786813+FrancsXiang@users.noreply.github.com>
2020-04-30 08:30:58 +0800

    Update min_edit_dist.cpp

--- dp/min_edit_dist.cpp
+++ dp/min_edit_dist.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 int len[MAXN][MAXN];
 
-//minimun distance from s1 to s2;
+//minimum distance from s1 to s2;
 int main()
 {
 	int mini;
 	string s1, s2;
 	cin >> s1 >> s2;
-	for (int i = 0; i < s1.length(); i++) len[i + 1][0] = i + 1;
-	for (int i = 0; i < s2.length(); i++) len[0][i + 1] = i + 1;
+	for (int i = 1; i <= s1.length(); i++) len[i][0] = i;
+	for (int i = 1; i <= s2.length(); i++) len[0][i] = i;
 	for (int i = 1; i <= s1.length(); i++) {
 		for (int j = 1; j <= s2.length(); j++) {
 			mini = min(len[i][j - 1] + 1, len[i - 1][j] + 1);

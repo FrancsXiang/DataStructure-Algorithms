0f70d29b1e76cd7c3c2f60abe530699a9ffd5eaf
Franc_zi <31786813+FrancsXiang@users.noreply.github.com>
2020-04-30 20:18:57 +0800

    Create kmp.cpp

---
+++ string/kmp.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+//referred_docs:https://wiki.jikexueyuan.com/project/kmp-algorithm/define.html
+
+
+//next[i] shows longest_pre_suf_pair_length between 0 to i-1.
+void getNext(string m,vector<int>& next) {
+	int i = 0, k = -1;
+	next[i] = -1; // next[0] == -1 means no elem front it
+	while (i < m.length() - 1) {
+		if (k == -1 || m[i] == m[k]) { 
+			k++;
+			i++;
+			next[i] = k;
+		}
+		else
+			k = next[k]; //divide smaller symmetrical block to find the most bigger pair.
+	}
+}
+
+int kmp(string s,string m) {
+	vector<int> next(m.size());
+	getNext(m,next);
+	int i = 0, j = 0;
+	int len1 = s.length(), len2 = m.length();
+	while (i < len1 && j < len2) {
+		//if j == -1 means no elem could be used to pair the s[i].
+		// just rest m and go head with index i!
+		if (j == -1 || s[i] == m[j]) {
+			i++;
+			j++;
+		}
+		else
+			j = next[j];// adjust the index j
+	}
+	if (j == len2) return i - j;
+	else return -1;
+}

6da2d62530bf6a897f2ec1746361fcda6659bee1
Franc_zi <31786813+FrancsXiang@users.noreply.github.com>
2020-05-04 12:08:21 +0800

    Update functor_sample.cpp

--- stl/functor_sample.cpp
+++ stl/functor_sample.cpp
@@ -11,8 +11,8 @@ using namespace std;
 
 template<typename T>
 struct cmp {
-	bool operator() (const T& a, const T& b) {
-		return a < b;
+	bool operator() (const T& a, const T& b, const T& thres) {
+		return a < b && a < thres;
 	}
 };
 
@@ -28,8 +28,8 @@ private:
 };
 
 template<typename T, typename F>
-bool calc(T a, T b, F fc) {
-	return fc(a, b);
+bool calc(T a, T b, T c, F fc) {
+	return fc(a, b, c);
 }
 
 template<typename T, typename F>
@@ -38,21 +38,21 @@ class test
 public:
 	F func;
 	T getMaxi();
-	test(vector<T>,F);
+	test(vector<T>, F);
 private:
 	T maxi;
 	vector<T> con;
 };
 
-template<typename T,typename F>
-T test<T,F>::getMaxi() {
+template<typename T, typename F>
+T test<T, F>::getMaxi() {
 	maxi = MIN_INF;
 	for (auto& it : con) maxi = func(maxi, it);
 	return maxi;
 }
 
 template<typename T, typename F>
-test<T,F>::test(vector<T> con, F func) {
+test<T, F>::test(vector<T> con, F func) {
 	this->con = con;
 	this->func = func;
 	maxi = MIN_INF;
@@ -63,14 +63,16 @@ int main()
 	srand(time(0));
 	int x = 3;
 	int y = 4;
+	int z = 5;
 	string a = "a";
 	string b = "b";
+	string c = "z";
 	vector<int> v1;
 	vector<string> v2;
-	cout << calc<int, cmp<int>>(x, y, cmp<int>()) << endl;
-	cout << calc<string, cmp<string>>(a, b, cmp<string>()) << endl;
+	cout << calc<int, cmp<int>>(x, y, z, cmp<int>()) << endl;
+	cout << calc<string, cmp<string>>(a, b, c, cmp<string>()) << endl;
 	for (int i = 0; i < 10; i++) v1.push_back(rand() % 1000 + 1);
-	test<int, _cmp<int>> judge(v1,_cmp<int>());
+	test<int, _cmp<int>> judge(v1, _cmp<int>());
 	cout << judge.getMaxi() << " " << judge.func.get_state() << endl;
 	return 0;
 }

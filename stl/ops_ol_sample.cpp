094ee993aea6793b893a789226107ebf5d33993b
Franc_zi <31786813+FrancsXiang@users.noreply.github.com>
2020-05-04 20:40:09 +0800

    Create ops_ol_sample.cpp

---
+++ stl/ops_ol_sample.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+// (++) / (--) / (+) / (-) / (*) / (/) / (cout) / (cin) ops overloading sample of INT class
+class INT
+{
+	friend ostream& operator<< (ostream& out, const INT& elem);
+	friend istream& operator>> (istream& in, const INT& elem);
+public:
+	INT(int i) : m_i(i) {}
+	INT& operator++() {
+		++(this->m_i);
+		return *this;
+	}
+	const INT operator++(int) {
+		INT tmp = *this;
+		++(*this);
+		return tmp;
+	}
+	int& operator*() const {
+		return (int&)m_i;
+	}
+	INT operator+(INT& b) {
+		return INT(this->m_i + b.m_i);
+	}
+	INT operator-(INT& b) {
+		return INT(this->m_i - b.m_i);
+	}
+	INT operator*(INT& b) {
+		return INT(this->m_i * b.m_i);
+	}
+	INT operator/(INT& b) {
+		return INT(this->m_i / b.m_i);
+	}
+private:
+	int m_i;
+};
+
+
+ostream& operator<< (ostream& out, const INT& elem) {
+	out << elem.m_i;
+	return out;
+}
+
+istream& operator>> (istream& in, const INT& elem) {
+	in >> elem.m_i;
+	return in;
+}
+
+int main()
+{
+	INT a(2), b(2);
+	auto c = a++;
+	cout << *a << " " << *c << endl;
+	cout << a++ << endl;
+	cout << ++c << endl;
+	cout << a << " " << b << endl;
+	cout << a / b << endl;
+	return 0;
+}

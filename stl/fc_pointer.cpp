c63529dd89c2eceaa2de7642cc3c6d1bb9ea77da
Franc_zi <31786813+FrancsXiang@users.noreply.github.com>
2020-05-06 18:14:07 +0800

    Update fc_pointer.cpp

--- stl/fc_pointer.cpp
+++ stl/fc_pointer.cpp
@@ -2,18 +2,25 @@
 #include <cstring>
 using namespace std;
 
-typedef int (*fp) ();
+typedef void (*fp) ();
 
-int add() {
+void add() {
 	cout << "add" << endl;
 }
 
-int (*set_malloc_handler(int (*f) ())) () {
-	int (*fp) () = add;
+void (*set_malloc_handler(void (*f) ())) () {
+	void (*fp) () = add;
 	return fp;
 }
 
 fp set_malloc_handler_beta(fp f) {
 	fp tmp = add;
 	return tmp;
+}
+
+int main()
+{
+	auto fc_1 = set_malloc_handler(add);
+	auto fc_2 = set_malloc_handler_beta(add);
+	fc_1(); fc_2();
 }

301770aef60a230bb9091e1c6e1fd51679584208
Franc_zi <31786813+FrancsXiang@users.noreply.github.com>
2020-05-10 19:47:54 +0800

    Update construct.h

--- stl/alloc/construct.h
+++ stl/alloc/construct.h
@@ -1,17 +1,17 @@
 #include <new>
 
-template<class T1, class T2>
+template<typename T1, typename T2>
 inline void construct(T1* p, const T2& value) {
 	new ((void*)p) T1(value);
 }
 
-template<class T1, class T2>
-inline void construct(T1* p, const T2& value) {
+template<typename T1, typename T2>
+inline void construct(T1* p) {
 	new ((void*)p) T1();
 }
 
 
-template<class T>
+template<typename T>
 inline void destroy(T* pointer) {
 	pointer->~T();
 }
@@ -23,24 +23,53 @@ inline void destroy(float*, float*) {}
 inline void destroy(double*, double*) {}
 inline void destroy(wchar_t*,wchar_t*) {}
 
-template<class ForwardIterator>
-inline void destroy(ForwardIterator first, ForwardIterator last) {
-	__destroy(first, last, value_type(first));
+struct __true_type {};
+struct __false_type {};
+
+template<typename Iterator>
+struct iterator_traits_ {
+	typedef typename Iterator::value_type value_type;
+};
+
+template<typename T>
+struct iterator_traits_<T*> {
+	typedef typename T value_type;
+};
+
+template<typename T>
+struct iterator_traits_<const T*> {
+	typedef typename T value_type;
+};
+
+template<typename T>
+struct __type_traits_ {
+	typedef typename __false_type has_trivial_destructor;
+};
+
+template<>
+struct __type_traits_<char>{ //POD type example.
+	typedef typename __true_type has_trivial_destructor;
+};
+
+template<typename forward_iterator>
+inline void destroy(forward_iterator first, forward_iterator last) {
+	__destroy(first, last, iterator_traits_<first>::value_type());
 }
 
-template<class ForwardIterator, class T>
-inline void __destroy(ForwardIterator first, ForwardIterator last, T*) {
+template<typename forward_iterator, typename T>
+inline void __destroy(forward_iterator first, forward_iterator last, T) {
 	typedef typename __type_traits_<T>::has_trivial_destructor _Trivial_destructor;
 	__destroy_aux(first, last, _Trivial_destructor());
 }
 
-template<class ForwardIterator>
-inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __false_type) {
+template<typename forward_iterator>
+inline void __destroy_aux(forward_iterator first, forward_iterator last, __false_type) {
 	while (first != last) {
 		destroy(&*first);
 		first++;
 	}
 }
 
-template<class ForwardIterator>
-inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __true_type) {}
+template<typename forward_iterator>
+inline void __destroy_aux(forward_iterator first, forward_iterator last, __true_type) {}
+

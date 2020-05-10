8e3c269cb700dd91a269cf68f98f33a56b59186f
Franc_zi <31786813+FrancsXiang@users.noreply.github.com>
2020-05-10 11:03:05 +0800

    Update iter_sample.cpp

--- stl/iter/iter_sample.cpp
+++ stl/iter/iter_sample.cpp
@@ -6,7 +6,7 @@ template<typename T>
 class Item
 {
 public:
-	Item(T _val):val(_val), next(NULL) {};
+	Item(T _val) :val(_val), next(NULL) {};
 	T get_val() const { return val; }
 	Item<T>*& get_next() { return next; }
 private:
@@ -60,6 +60,7 @@ void List<T>::display(std::ostream& os) const {
 
 template<typename T>
 struct Iter {
+	typedef T value_type;
 	T* ptr;
 	Iter(T* p = 0) : ptr(p) {};
 	T& operator*() const { return *ptr; }
@@ -75,7 +76,7 @@ bool operator==(const Item<T>& item, T val) {
 	return item.get_val() == val;
 }
 
-template<typename InputIterator,typename T>
+template<typename InputIterator, typename T>
 InputIterator search(InputIterator first, InputIterator last, const T val) {
 	while (first != last) {
 		if (*first == val) break;
@@ -83,6 +84,27 @@ InputIterator search(InputIterator first, InputIterator last, const T val) {
 	}
 	return first;
 }
+
+template<typename I>
+struct iterator_traits_ {
+	typedef typename I::value_type value_type;
+};
+
+template<typename T>
+struct iterator_traits_<T*> {
+	typedef T value_type;
+};
+
+template<typename T>
+struct iterator_traits_<const T*> {
+	typedef T value_type;
+};
+
+template<typename I>
+typename iterator_traits_<I>::value_type func(I it) {
+	return *it;
+}
+
 
 int main()
 {
@@ -94,8 +116,9 @@ int main()
 	list.display();
 	Iter<Item<int>> begin(list.get_front());
 	Iter<Item<int>> end;
-	auto iter = search<Iter<Item<int>>, int>(begin, end, 100);
+	auto iter = search(begin, end, 18);
 	if (iter == end) cout << "no elem found!" << endl;
 	else cout << "elem found: " << iter->get_val() << endl;
+	cout << func(begin).get_val() << endl;
 	return 0;
 }
